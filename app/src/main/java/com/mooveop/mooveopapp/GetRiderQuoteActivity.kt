4293a6dc35956c7fdfa845fdb55a97f06b4e63fb
karan <karan26.ahuja@gmail.com>
2021-12-16 03:47:02 +0530

    add

--- app/src/main/java/com/mooveop/mooveopapp/GetRiderQuoteActivity.kt
+++ app/src/main/java/com/mooveop/mooveopapp/GetRiderQuoteActivity.kt
@@ -29,15 +29,21 @@ import com.google.android.libraries.places.api.model.TypeFilter
 import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
 import com.google.android.libraries.places.api.net.FindAutocompletePredictionsResponse
 import kotlin.math.roundToInt
-
+data class RiderRow(
+    var toLocationSelectedText:String = "",
+    var hoursText:String = "00",
+    var minutesText:String = "00",
+    var myIndex:Int = 0
+)
 class GetRiderQuoteActivity : ComponentActivity() {
     var myPlacesData = mutableMapOf <String,Int?>()
     var totalCost = mutableStateOf(0)
     var previousTextLength = 0
-    var hoursText = mutableStateOf("00")
-    var minutesText = mutableStateOf("00")
-    var riderCount = mutableStateOf (1)
-
+    var hoursText = mutableStateListOf ("00")
+    var minutesText = mutableStateListOf("00")
+    var selectedText = mutableStateListOf("")
+    var riderCount = mutableStateOf (0)
+    var riderRowsList = mutableStateListOf<RiderRow>()
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Initialize the SDK
@@ -53,7 +59,14 @@ class GetRiderQuoteActivity : ComponentActivity() {
         )
 //        https://developers.google.com/maps/documentation/places/android-sdk/start#maps_places_get_started-kotlin
 //        https://developers.google.com/maps/documentation/places/android-sdk/autocomplete#maps_places_programmatic_place_predictions-kotlin
-
+        fun updateCurrentRowHoursText(theRowIndex:Int,theHoursText:String)
+{
+    println ("the index is" )
+    println (theRowIndex)
+    println (riderRowsList[0])
+    println (riderRowsList[1])
+            riderRowsList[theRowIndex] = riderRowsList[theRowIndex].copy(hoursText = theHoursText)
+        }
         setContent {
             var expanded by remember { mutableStateOf(false) }
             var hoursExpanded by remember { mutableStateOf(false) }
@@ -64,14 +77,29 @@ class GetRiderQuoteActivity : ComponentActivity() {
             var minutesList = remember { mutableStateListOf <String>("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60") }
             totalCost = remember {totalCost}
             riderCount = remember {riderCount}
-            var hoursText by remember {hoursText}
-            var minutesText by remember {minutesText}
-            var selectedText by remember { mutableStateOf("") }
+             hoursText = remember {hoursText}
+            riderRowsList = remember {riderRowsList}
+            var minutesText = remember {minutesText}
+            var selectedText = remember {selectedText}
             var dropDownWidth by remember { mutableStateOf(0) }
             Scaffold(
                 floatingActionButton={
                     FloatingActionButton(
-                        onClick = {riderCount.value +=1},
+                        onClick = {
+                            riderCount.value +=1
+//                            hoursText.add("00")
+//                            minutesText.add("00")
+//                            selectedText.add("")
+                                  riderRowsList.add (
+                                      RiderRow(
+                                          "",
+                                          "00",
+                                          "00",
+                                          riderCount.value - 1
+
+                                      )
+                                          )
+                                  },
                         content={
                             Icon(
                                 imageVector = Icons.Outlined.Add,
@@ -81,15 +109,15 @@ class GetRiderQuoteActivity : ComponentActivity() {
 
                     )} ,
                     content={Column() {
-                        for (i in 1..riderCount.value) {
+                        var loopCounter = 0
+                        repeat (riderRowsList.size) {
                             OutlinedTextField(
-                                value = selectedText,
+                                value = riderRowsList[loopCounter].toLocationSelectedText,
                                 onValueChange = {
-                                    previousTextLength = selectedText.length
-                                    selectedText = it
+                                    previousTextLength =  riderRowsList[loopCounter].toLocationSelectedText.length
+                                    riderRowsList[loopCounter].toLocationSelectedText = it
                                     println("inside value change")
                                     if (it.length > previousTextLength && it.length > 2) {
-
                                         val request =
                                             FindAutocompletePredictionsRequest.builder()
                                                 // Call either setLocationBias() OR setLocationRestriction().
@@ -169,14 +197,14 @@ class GetRiderQuoteActivity : ComponentActivity() {
                                 suggestions.forEach { label ->
                                     DropdownMenuItem(onClick = {
 
-                                        selectedText = label
+                                        riderRowsList[loopCounter].toLocationSelectedText= label
                                         println(myPlacesData)
                                         println(label)
 
-                                        println((myPlacesData.get(selectedText)!! * 0.014).roundToInt())
+                                        println((myPlacesData.get( riderRowsList[loopCounter].toLocationSelectedText)!! * 0.014).roundToInt())
 
                                         totalCost.value =
-                                            (myPlacesData.get(selectedText)!! * 0.014).roundToInt()
+                                            (myPlacesData.get( riderRowsList[loopCounter].toLocationSelectedText)!! * 0.014).roundToInt()
                                     }) {
                                         Text(text = label)
                                     }
@@ -184,52 +212,11 @@ class GetRiderQuoteActivity : ComponentActivity() {
                             }
                             Spacer(modifier = Modifier.height(20.dp))
                             Row() {
-                                OutlinedTextField(
-                                    value = hoursText,
-                                    onValueChange = { hoursText = it },
-                                    readOnly = true,
-                                    modifier = Modifier
-                                        .clickable { hoursExpanded = true }
-                                        .width(140.dp)
-                                        .padding(start = 20.dp, end = 20.dp),
-                                    trailingIcon = {
-                                        if (hoursExpanded == false) {
-                                            IconButton(onClick = { hoursExpanded = true }) {
-                                                Icon(
-                                                    imageVector = Icons.Outlined.ArrowDropDown,
-                                                    contentDescription = null
-                                                )
-                                            }
-                                        }
-                                        if (hoursExpanded == true) {
-                                            IconButton(onClick = { hoursExpanded = false }) {
-                                                Icon(
-                                                    imageVector = Icons.Outlined.ArrowBack,
-                                                    contentDescription = null
-                                                )
-                                            }
-                                        }
-                                    }
-                                )
-                                DropdownMenu(
-                                    expanded = hoursExpanded,
-                                    onDismissRequest = { hoursExpanded = false },
-                                    properties = PopupProperties(focusable = false),
-                                    modifier = Modifier.fillMaxWidth()
-                                ) {
-                                    hoursList.forEach { label ->
-                                        DropdownMenuItem(onClick = {
-                                            hoursText = label
-                                            hoursExpanded = false
-                                        }) {
-                                            Text(text = label)
-                                        }
-                                    }
-                                }
+
                                 //only minutesText andminutesExpanded are different below
                                 OutlinedTextField(
-                                    value = minutesText,
-                                    onValueChange = { minutesText = it },
+                                    value =  riderRowsList[loopCounter].minutesText,
+                                    onValueChange = {  riderRowsList[loopCounter].minutesText = it },
                                     readOnly = true,
                                     modifier = Modifier
                                         .clickable { minutesExpanded = true }
@@ -262,7 +249,7 @@ class GetRiderQuoteActivity : ComponentActivity() {
                                 ) {
                                     minutesList.forEach { label ->
                                         DropdownMenuItem(onClick = {
-                                            minutesText = label
+                                            riderRowsList[loopCounter].minutesText = label
                                             minutesExpanded = false
                                         }) {
                                             Text(text = label)
@@ -287,6 +274,54 @@ class GetRiderQuoteActivity : ComponentActivity() {
                     }}
                     )
                 }
+            @Composable
+            fun hoursField()
+            {
+                OutlinedTextField(
+                    value =  riderRowsList[loopCounter].hoursText,
+                    onValueChange = {
+                        updateCurrentRowHoursText ( riderRowsList[loopCounter].myIndex,it)
+                    },
+                    readOnly = true,
+                    modifier = Modifier
+                        .clickable { hoursExpanded = true }
+                        .width(140.dp)
+                        .padding(start = 20.dp, end = 20.dp),
+                    trailingIcon = {
+                        if (hoursExpanded == false) {
+                            IconButton(onClick = { hoursExpanded = true }) {
+                                Icon(
+                                    imageVector = Icons.Outlined.ArrowDropDown,
+                                    contentDescription = null
+                                )
+                            }
+                        }
+                        if (hoursExpanded == true) {
+                            IconButton(onClick = { hoursExpanded = false }) {
+                                Icon(
+                                    imageVector = Icons.Outlined.ArrowBack,
+                                    contentDescription = null
+                                )
+                            }
+                        }
+                    }
+                )
+                DropdownMenu(
+                    expanded = hoursExpanded,
+                    onDismissRequest = { hoursExpanded = false },
+                    properties = PopupProperties(focusable = false),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    hoursList.forEach { label ->
+                        DropdownMenuItem(onClick = {
+                            updateCurrentRowHoursText ( riderRowsList[loopCounter].myIndex,label)
+                            hoursExpanded = false
+                        }) {
+                            Text(text = label)
+                        }
+                    }
+                }
+            }
 
         }
     }
